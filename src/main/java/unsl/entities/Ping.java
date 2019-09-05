5e0fb375e6f1058af44c4e87a8047cab525d04d6
Jeremias Herrera <jrherrera>
2019-09-04 21:21:46 -0300

    limpio codigo

---
+++ src/main/java/unsl/entities/Ping.java
@@ -0,0 +1,18 @@
+package unsl.entities;
+
+public class Ping {
+
+	private String ping;
+
+	public Ping(String ping){
+		this.ping = ping;
+	}
+
+	public String getPing() {
+        return ping;
+    }
+
+    public void setPing(String ping) {
+        this.ping = ping;
+    }
+}
